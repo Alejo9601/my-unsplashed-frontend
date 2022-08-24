4855146d866e4ead1f9fc2ebdb41b7a60987baf7
Alejo9601 <alejandro96jm@gmail.com>
2022-08-24 20:23:01 -0300

    Added search icon

--- src/components/Header/SearchBar.js
+++ src/components/Header/SearchBar.js
@@ -1,7 +1,7 @@
 const SearchBar = () => {
   return (
     <form className="header__searchbar">
-      <div className="header__searchbar__icon">lasdasd</div>
+      <div className="header__searchbar__icon"></div>
       <input type="search" name="search" placeholder="Search by name" />
     </form>
   );
