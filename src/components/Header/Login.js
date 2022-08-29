6a922da048fab6ea79d868b5d6a4401123ff2d15
Alejo9601 <alejandro96jm@gmail.com>
2022-08-28 23:04:21 -0300

    Added auth  options to header

---
+++ src/components/Header/Login.js
@@ -0,0 +1,11 @@
+const Login = () => {
+  return (
+    <div className="header-auth-options">
+      <a href="">Login</a>
+      <span>{"  /  "}</span>
+      <a href="">Sign in</a>
+    </div>
+  );
+};
+
+export default Login;
