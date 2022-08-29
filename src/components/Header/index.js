76f978b4fe27aa17ce9b6db0eb9528e7de0a0764
Alejo9601 <alejandro96jm@gmail.com>
2022-08-28 23:06:11 -0300

    Using auth options

--- src/components/Header/index.js
+++ src/components/Header/index.js
@@ -2,13 +2,21 @@ import Logo from "./Logo";
 import SearchBar from "./SearchBar";
 import UploadBtn from "./UploadBtn";
 import "../../styles/header.css";
+import { useState } from "react";
+import Login from "./Login";
 
 const Header = () => {
+  const [logged, setLogged] = useState(true);
+
   return (
     <header className="header">
       <Logo></Logo>
       <SearchBar></SearchBar>
-      <UploadBtn Style={{ alignSelf: "flex-end" }}></UploadBtn>
+      {logged ? (
+        <UploadBtn Style={{ alignSelf: "flex-end" }}></UploadBtn>
+      ) : (
+        <Login></Login>
+      )}
     </header>
   );
 };
