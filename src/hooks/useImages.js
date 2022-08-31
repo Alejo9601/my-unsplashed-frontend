2fe65088ad8023e3ab9027c3d313c25c32c2889b
Alejo9601 <alejandro96jm@gmail.com>
2022-08-31 18:00:02 -0300

    Fixed getting undefined when using hook

--- src/hooks/useImages.js
+++ src/hooks/useImages.js
@@ -2,13 +2,13 @@ import { useEffect, useState } from "react";
 import getAllImages from "../services/getAllImages";
 
 const useImages = () => {
-    const [images, setImages] = useState()
+  const [images, setImages] = useState([]);
 
-    useEffect(() => {
-        getAllImages().then(res => setImages(res))
-    }, [])
+  useEffect(() => {
+    getAllImages().then((res) => setImages(res));
+  }, []);
 
-    return images
-}
- 
+  return images;
+};
+
 export default useImages;
