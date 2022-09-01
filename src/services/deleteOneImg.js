87a8fe4f0874aec324673c2752fe07218e40fe45
Alejo9601 <alejandro96jm@gmail.com>
2022-09-01 14:23:14 -0300

    Fixed fetching on bad URL

--- src/services/deleteOneImg.js
+++ src/services/deleteOneImg.js
@@ -2,7 +2,7 @@ const deleteOneImg = (imgId) => {
   const options = {
     method: "DELETE",
   };
-  return fetch(`http://localhost:3001/api/v1:${imgId}`, options);
+  return fetch(`http://localhost:3001/api/v1/${imgId}`, options);
 };
 
 export default deleteOneImg;
