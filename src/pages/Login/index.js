25b7f04f6b28e612556d1440c308acc518e831cc
Alejo9601 <alejandro96jm@gmail.com>
2022-09-03 18:57:20 -0300

    Modified login page

--- src/pages/Login/index.js
+++ src/pages/Login/index.js
@@ -4,22 +4,24 @@ import logo from "../../assets/favicon.ico";
 const Login = () => {
   return (
     <div className="login-container">
-      <div className="login-header">
-        <img src={logo} alt="unsplashed logo" />
-        <h1>Login</h1>
-        <span>Welcome</span>
+      <div className="login-card">
+        <div className="login-card__header">
+          <img src={logo} alt="unsplashed logo" />
+          <h1>Login</h1>
+          <span>Welcome</span>
+        </div>
+        <form className="login-card__form">
+          <label>
+            User
+            <input id="username" type="text" name="username" />
+          </label>
+          <label>
+            Password
+            <input id="userpass" type="password" name="userpass" />
+          </label>
+          <input id="login-btn" type="button" name="loginbtn" value="Login" />
+        </form>
       </div>
-      <form className="login-form">
-        <label>
-          User
-          <input id="username" type="text" name="username" />
-        </label>
-        <label>
-          Password
-          <input id="userpass" type="password" name="userpass" />
-        </label>
-        <input id="login-btn" type="button" name="loginbtn" value="Login" />
-      </form>
     </div>
   );
 };
