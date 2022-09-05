39f98dc2ad8d15711167adc0ded2c26977a8ded1
Alejo9601 <alejandro96jm@gmail.com>
2022-09-04 23:02:37 -0300

    Deleted useless imports and fixed bad hook usage

--- src/components/Header/index.js
+++ src/components/Header/index.js
@@ -2,18 +2,17 @@ import Logo from "./Logo";
 import SearchBar from "./SearchBar";
 import UploadBtn from "./UploadBtn";
 import "../../styles/header.css";
-import { useState } from "react";
 import Login from "./Login";
 import useUser from "../../hooks/useUser";
 
 const Header = ({ handleClickUpload }) => {
-  const isLogged = useUser();
+  const { user } = useUser();
 
   return (
     <header className="header">
       <Logo></Logo>
       <SearchBar></SearchBar>
-      {isLogged ? (
+      {user.logged ? (
         <UploadBtn handleUpload={handleClickUpload} />
       ) : (
         <Login></Login>
