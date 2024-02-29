760b4ad23bff4a6cef0dfb4a318f46b2cc8bec94
Alejo9601 <alejandro96jm@gmail.com>
2024-02-28 22:36:55 -0300

    Deleted inused imports

--- src/components/Header/index.js
+++ src/components/Header/index.js
@@ -2,24 +2,23 @@ import Logo from "./Logo";
 import SearchBar from "./SearchBar";
 import UploadBtn from "./UploadBtn";
 import "../../styles/header.css";
-import { useState } from "react";
 import Login from "./Login";
 import useUser from "../../hooks/useUser";
 
 const Header = ({ handleClickUpload }) => {
-  const isLogged = useUser();
+   const isLogged = useUser();
 
-  return (
-    <header className="header">
-      <Logo></Logo>
-      <SearchBar></SearchBar>
-      {isLogged ? (
-        <UploadBtn handleUpload={handleClickUpload} />
-      ) : (
-        <Login></Login>
-      )}
-    </header>
-  );
+   return (
+      <header className="header">
+         <Logo></Logo>
+         <SearchBar></SearchBar>
+         {isLogged ? (
+            <UploadBtn handleUpload={handleClickUpload} />
+         ) : (
+            <Login></Login>
+         )}
+      </header>
+   );
 };
 
 export default Header;
