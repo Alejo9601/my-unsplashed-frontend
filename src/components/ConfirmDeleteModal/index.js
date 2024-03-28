f2fd4a4d6aa5724baecd08c1ad83adf4c6834d49
Alejo9601 <alejandro96jm@gmail.com>
2024-03-28 13:17:40 -0300

    Modified delete modal

--- src/components/ConfirmDeleteModal/index.js
+++ src/components/ConfirmDeleteModal/index.js
@@ -2,30 +2,28 @@ import { useContext } from "react";
 import ImageToDeleteContext from "../../context/ImageToDeleteContext";
 import useImages from "../../hooks/useImages";
 import Modal from "../Generics/Modal";
+import { OpacityContainer } from "../../styles/styled/div";
 
 const ConfirmDeleteModal = ({ showModal }) => {
-  const { imgId, setImgId } = useContext(ImageToDeleteContext);
-  const { deleteImg } = useImages();
+   const { imgId, setImgId } = useContext(ImageToDeleteContext);
+   const { deleteImg } = useImages();
 
-  const handleOnConfirmDelete = () => {
-    deleteImg(imgId).then(showModal(false));
-  };
+   const handleOnConfirmDelete = () => {
+      deleteImg(imgId).then(showModal(false));
+   };
 
-  return (
-    <Modal
-      btnActionText="Delete"
-      handleAction={handleOnConfirmDelete}
-      handleCancel={() => {
-        showModal(false);
-        setImgId(null);
-      }}
-    >
-      <label>
-        Password
-        <input type="password" name="userpassword" />
-      </label>
-    </Modal>
-  );
+   return (
+      <OpacityContainer>
+         <Modal
+            btnActionText="Delete"
+            handleAction={handleOnConfirmDelete}
+            handleCancel={() => {
+               showModal(false);
+               setImgId(null);
+            }}
+         ></Modal>
+      </OpacityContainer>
+   );
 };
 
 export default ConfirmDeleteModal;
